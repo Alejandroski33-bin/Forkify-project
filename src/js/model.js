bb80e216de668f91afaab9d92efc6099eddce10e
Alejandroski33-bin <alecafer99@gmail.com>
2026-03-16 12:49:11 -0600

    Cleaned up controller

--- src/js/model.js
+++ src/js/model.js
@@ -49,7 +49,6 @@ export const loadSearchResults = async function (query) {
     state.search.query = query;
 
     const data = await AJAX(`${API_URL}?search=${query}&key=${KEY}`);
-    console.log(data);
 
     state.search.results = data.data.recipes.map(rec => {
       return {
@@ -114,7 +113,6 @@ const init = function () {
   if (storage) state.bookmarks = JSON.parse(storage);
 };
 init();
-console.log(state.bookmarks);
 
 const clearBookmarks = function () {
   localStorage.clear('bookmarks');
