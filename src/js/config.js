fc9f2eb01ac678f87a204e4b3b11d8dc5be95ee2
Alejandroski33-bin <alecafer99@gmail.com>
2026-05-12 23:23:39 -0600

    updated files

--- src/js/config.js
+++
@@ -1,4 +0,0 @@
-export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10;
-export const KEY = '8e7a1acd-ed0c-46d7-ae51-421f7c6c204b';
